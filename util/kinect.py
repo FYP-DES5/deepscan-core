04819dd2b5b81f4aab2206456fda3934562aeb76
Kristopher Lam <kris@sandb0x.io>
2017-04-09 18:21:32 +0800

    improve kinect.py

--- util/kinect.py
+++ util/kinect.py
@@ -62,9 +62,8 @@ def getRegister(zeroImage, blurValue = 5, threshold = 170):
     # points = [None] * (512 * 424)
     points = []
     tcoords = []
-    diffPoints = np.zeros_like(color_depth_map, dtype=np.uint8)
-    diff = np.zeros_like((424, 512), dtype=int)
-    mapping = np.zeros((424, 512, 2), dtype=int)
+    diffPoints = np.zeros((424, 512), dtype=np.uint8)
+    diff = np.zeros((424, 512), dtype=int)
     for i in range(424):
         for j in range(512):
             colorId = color_depth_map[512 * i + j]
@@ -72,7 +71,6 @@ def getRegister(zeroImage, blurValue = 5, threshold = 170):
             y = colorId / 1920
             if colorId == -1:
                 continue
-            mapping[i][j] = y, x
             difference = np.linalg.norm(np.zeros_like(color[y][x], dtype=float)
                     + color[y][x] - zeroImage[y][x])
             diff[i][j] = difference
@@ -80,20 +78,23 @@ def getRegister(zeroImage, blurValue = 5, threshold = 170):
                 continue
             diffPoints[i][j] = 255
     kernel = np.ones((3, 3), dtype=np.uint8)
-    numpy.savetxt('diffPoints.txt', diffPoints)
-    numpy.savetxt('diff.txt', diff)
-    cv2.erode(diff, kernel, iterations=3)
-    cv2.dilate(diff, kernel, iterations=6)
-    cv2.erode(diff, kernel, iterations=3)
-    numpy.savetxt('diffPoints_after.txt', diffPoints)
+    np.savetxt('/Users/admin/Desktop/diffPoints.txt', diffPoints)
+    np.savetxt('/Users/admin/Desktop/diff.txt', diff)
+    cv2.erode(diffPoints, kernel, diffPoints, iterations=3)
+    cv2.dilate(diffPoints, kernel, diffPoints, iterations=6)
+    cv2.erode(diffPoints, kernel, diffPoints, iterations=3)
+    np.savetxt('/Users/admin/Desktop/diffPoints_after.txt', diffPoints)
     counter = 0
     for i in range(424):
         for j in range(512):
-            if diffPoints[i][j] == 255:
-                point = registration.getPointXYZ(undistorted, i, j)
-                tcoord = (x / 1920.0, 1 - y / 1080.0)
-                points.append(point)
-                tcoords.append(tcoord)
+            colorId = color_depth_map[512 * i + j]
+            if diffPoints[i][j] == 255 and colorId != -1:
+                if not i % 10 and not j % 10:
+                    point = registration.getPointXYZ(undistorted, i, j)
+                    x, y = colorId % 1920, colorId / 1920
+                    tcoord = (x / 1920.0, 1 - y / 1080.0)
+                    points.append(point)
+                    tcoords.append(tcoord)
     return color, points, tcoords
 
 
