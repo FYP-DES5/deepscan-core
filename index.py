0f39b10d0f4c5891fb9754a30cd52e74b266ec25
Toby Tse <chtseac@ust.hk>
2017-04-13 19:21:01 +0800

    opcode echo

--- index.py
+++ index.py
@@ -15,30 +15,30 @@ class Server:
         try:
             while 1:
                 print 'waiting for data...'
-                opcode = self.conn.recv(1)
-                print 'data: %s' % opcode
-                if not opcode:
+                self.opcode = self.conn.recv(1)
+                print 'data: %s' % self.opcode
+                if not self.opcode:
                     break
-                opcode = ord(opcode[0])
-                print opcode is 1, opcode is 2, opcode is 3, opcode is 4, opcode is 5, opcode is 6
-                print opcode == 1, opcode == 2, opcode == 3, opcode == 4, opcode == 5, opcode == 6
-                if opcode is 1: # start
+                self.opcode = ord(self.opcode[0])
+                print self.opcode is 1, self.opcode is 2, self.opcode is 3, self.opcode is 4, self.opcode is 5, self.opcode is 6
+                print self.opcode == 1, self.opcode == 2, self.opcode == 3, self.opcode == 4, self.opcode == 5, self.opcode == 6
+                if self.opcode is 1: # start
                     if not self.started:
                         core.start()
                         self.sendSuccess()
                     else:
                         self.sendFail()
-                elif opcode is 2: # end
+                elif self.opcode is 2: # end
                     if self.started:
                         kinect.stop()
                     self.sendSuccess()
-                elif opcode is 3: # zero
+                elif self.opcode is 3: # zero
                     if not self.started:
                         self.sendFail()
                     else:
                         core.zero()
                         self.sendSuccess()
-                elif opcode is 4: # scan
+                elif self.opcode is 4: # scan
                     if not self.started:
                         self.sendFail()
                     else:
@@ -46,7 +46,7 @@ class Server:
                         arr = np.arange(3*15*10, dtype=np.uint8).reshape((10, 15, 3))
                         self.sendSuccess()
                         self.sendArr(arr)
-                elif opcode is 5: # get image
+                elif self.opcode is 5: # get image
                     if not self.started:
                         self.sendFail()
                     else:
@@ -54,15 +54,17 @@ class Server:
                         arr = np.arange(3*15*10, dtype=np.uint8).reshape((10, 15, 3))
                         self.sendSuccess()
                         self.sendArr(arr)
-                elif opcode is 6: # start visualizer
+                elif self.opcode is 6: # start visualizer
                     self.sendSuccess()
                     mockVisualizer.start()
         finally:
             self.conn.close()
     def sendSuccess(self):
         self.conn.send(np.uint8(0).tobytes())
+        self.conn.send(np.uint8(self.opcode).tobytes())
     def sendFail(self):
         self.conn.send(np.uint8(1).tobytes())
+        self.conn.send(np.uint8(self.opcode).tobytes())
     def sendArr(self, arr):
         self.conn.send(np.array(arr.shape, dtype=np.uint32).tobytes('C'))
         self.conn.send(arr.tobytes('C'))
