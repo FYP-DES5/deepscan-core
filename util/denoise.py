9ed56f73564300ea5471bb561dac1fd466aa4deb
Toby Tse <chtseac@ust.hk>
2017-04-13 18:13:26 +0800

    edge voxel

--- util/denoise.py
+++ util/denoise.py
@@ -1,21 +1,50 @@
 import cv2
-import gdfmm
+# import gdfmm
+import sys
 import numpy as np
 
 def voxelGridFilter(points, tcoords, gridsize=0.01):
 	voxels = {}
 	print len(points)
+	maxX, maxY, minX, minY = sys.maxint, sys.maxint, -sys.maxint - 1, -sys.maxint - 1
 	for i in range(len(points)):
-		n = tuple(map(lambda x: round(x / gridsize),np.copy(points[i]))[0:2])
+		n = tuple(map(lambda x: int(round(x / gridsize)),np.copy(points[i]))[0:2])
 		if n not in voxels:
 			voxels[n] = []
+			minX, maxX = max(n[0], minX), min(n[0], maxX)
+			minY, maxY = max(n[1], minY), min(n[1], maxY)
 		voxels[n].append({
 			"p" : points[i],
 			"t" : tcoords[i]
 			})
-	rp = map(lambda n: np.average(np.array(map(lambda e: e["p"], voxels[n])), axis=0), voxels)
-	rt = map(lambda n: np.average(np.array(map(lambda e: e["t"], voxels[n])), axis=0), voxels)
-	print len(rp)
+	mask = np.zeros((2 + maxX - minX, 2 + maxY - minY), dtype=np.uint8)
+	mask[np.array(voxels.keys()) + (1 - minX, 1 - minY)] = 255
+	contours, hierachy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	largestContour = max(contours, key=cv2.contourArea)
+	targetPoints = int(round(8 * len(voxels) ** 0.5))
+	contourPoints = []
+	for i in range(len(largestContour)):
+		p1 = largestContour[i - 1][0]
+		p2 = largestContour[i][0]
+		diff = p2 - p1
+		mag = max(*[abs(n) for n in diff])
+		step = np.array(diff) / mag
+		for j in range(mag):
+			contourPoints.append(tuple(p1 + diff * j))
+	for point in contourPoints:
+		v = voxels[point]
+		q1, q2, q3, q4 = [], [], [], []
+		for e in v:
+			((q1 if e["p"][0] > point[0] else q2)
+				if e["p"][1] > point[1] else
+			 (q4 if e["p"][0] > point[0] else q3)).append(e)
+		voxels[(point, 'q1')] = q1
+		voxels[(point, 'q2')] = q2
+		voxels[(point, 'q3')] = q3
+		voxels[(point, 'q4')] = q4
+		del voxels[point]
+	rp = [np.average(np.array([e["p"] for e in voxels[n]]), axis=0) for n in voxels]
+	rt = [np.average(np.array([e["t"] for e in voxels[n]]), axis=0) for n in voxels]
 	return rp, rt
 
 def inpaint(bgr, depth):
